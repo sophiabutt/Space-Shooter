8ee27d225a6d8f58383c602b473460bf492a7d8a
sophiabutt <buttso@iu.edu>
2023-09-19 23:08:58 -0400

    9/19

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,8 +1,23 @@
 extends CharacterBody2D
 
 var Bullet = load("res://Enemy/Enemy_Bullet.tscn")
+var health = 10
+var y_positions = [100,150,200,500,550]
+var initial_position = Vector2.ZERO
+var direction = Vector2(1.5,0)
+var wobble = 30.0
 
 
+func _ready():
+	initial_position.x = -100
+	initial_position.y = y_positions[randi() % y_positions.size()]
+	position = initial_position
+	
+func _physics_process(_delta):
+		position += direction
+		position.y = initial_position.y + sin(position.x/20)*wobble
+		if position.x > 1200:
+				queue_free()
 
 
 func _on_timer_timeout():
@@ -15,8 +30,13 @@ func _on_timer_timeout():
 		bullet.global_position = global_position + Vector2(0,-40).rotated(d)
 		Effects.add_child(bullet)
 
+func damage(d):
+	health -= d
+	if health <= 0:
+		queue_free()
 
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
+		damage(100)
 		body.damage(100)
